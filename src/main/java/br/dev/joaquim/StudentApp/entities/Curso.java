41378e77308b398ab7f7766a41100124378cd6a8
Lucas Kato <143275020+Katojinnn@users.noreply.github.com>
2024-10-10 19:57:56 -0300

    Curso.java

--- src/main/java/br/dev/joaquim/StudentApp/entities/Curso.java
+++ src/main/java/br/dev/joaquim/StudentApp/entities/Curso.java
@@ -1,1 +1,42 @@
+package br.dev.joaquim.StudentApp.entities;
 
+public class Curso {
+  private int idCurso;
+  private String nomeCurso;
+  private String periodoCurso;
+
+  public Curso() {
+  }
+
+  public Curso(int idCurso, String nomeCurso, String periodoCurso) {
+    this.idCurso= idCurso;
+    this.nomeCurso = nomeCurso;
+  }
+
+  public int getIdCurso() {
+    return idCurso;
+  }
+
+  public String getNomeCurso() {
+    return nomeCurso;
+  }
+  public String getPeriodoCurso(){
+    return periodoCurso;
+  }
+
+  public void setIdCurso(int idCurso) {
+    this.idCurso = idCurso;
+  }
+
+  public void setNomeCurso(String nomeCurso) {
+    this.nomeCurso = nomeCurso;
+  }
+  public void setPeriodoCurso(String periodoCurso){
+    this.periodoCurso = periodoCurso;
+  }
+
+  @Override
+  public String toString() {
+    return "Curso " + getNomeCurso() + " [" + getIdCurso() + "]+[" + getPeriodoCurso() + "]";
+  }
+}
