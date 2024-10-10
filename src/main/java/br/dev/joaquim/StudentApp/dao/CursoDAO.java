637f1daf35b782bea943bdb809e6b552795f56b7
Lucas Kato <143275020+Katojinnn@users.noreply.github.com>
2024-10-10 20:17:14 -0300

    CursoDAO.java

--- src/main/java/br/dev/joaquim/StudentApp/dao/CursoDAO.java
+++ src/main/java/br/dev/joaquim/StudentApp/dao/CursoDAO.java
@@ -1,1 +1,17 @@
+package br.dev.joaquim.StudentApp.dao;
 
+import java.util.List;
+
+import br.dev.joaquim.StudentApp.entities.Curso;
+
+public interface CursoDAO {
+  public boolean create(Curso curso);
+
+  public List<Curso> findAll();
+
+  public Curso findByIdCurso(int idCurso);
+
+  public boolean update(Curso curso);
+
+  public boolean delete(int idCurso);
+}
